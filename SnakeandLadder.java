4f6e6d040869b50f4ebae6779296a49b1fd1c0d3
NayanTr09 <nayantripathi42@gmail.com>
2021-12-26 23:23:54 +0530

    Added: 3 Options

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -16,3 +16,43 @@ public class SnakeandLadder {
         Random random = new Random();
         int dieRoll = random.nextInt(1, 7);
     }
+
+    //        function to play available options
+    static int playAvailableOptions(int playerOnePosition, int dieRoll){
+        Random random = new Random();
+        int availableOptions = random.nextInt(1, 4);
+        switch(availableOptions){
+            case 1 -> {
+                System.out.println("No Play: ");
+                System.out.println("Die face value: " + dieRoll);
+            }
+            case 2 -> {
+                System.out.println("Ladder: ");
+                System.out.println("Die face value: " + dieRoll);
+                if(playerOnePosition < 100){
+                    int currentPosition = playerOnePosition + dieRoll;
+                    if(currentPosition <= 100){
+                        playerOnePosition = currentPosition;
+                    }
+                    else{
+                        System.out.println("You need to get the exact die face value to reach near or exactly 100 and not more than that ");
+                    }
+                }
+            }
+            case 3 -> {
+                System.out.println("Snake: ");
+                System.out.println("Die face value: " + dieRoll);
+                if(playerOnePosition > 0){
+                    int currentPosition = playerOnePosition - dieRoll;
+                    if(currentPosition >= 0){
+                        playerOnePosition = currentPosition;
+                    }
+                    else{
+                        System.out.println("You can't go down any further.");
+                    }
+                }
+            }
+        }
+        return playerOnePosition;
+    }
+}
