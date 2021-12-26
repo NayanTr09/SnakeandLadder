5efe43d158700aa6008467839750b501d381306f
NayanTr09 <nayantripathi42@gmail.com>
2021-12-26 23:10:11 +0530

    Initialized starting position

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -2,4 +2,13 @@ import java.util.*;
 
 public class SnakeandLadder {
     System.out.println("Welcome to Snake and Ladder game");
-}
+    public static void main(String[] args) {
+        numberOfPlayers();
+    }
+
+    //    function to initialize the number of players
+    static void numberOfPlayers(){
+//        setting the player's start position to 0
+        int playerOnePosition = 0;
+        playGame(playerOnePosition);
+    }
